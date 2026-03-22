4cad9c003a29bdcaa674edc0c1f080fb458b1487
nheralamierd <camilabmartinez7@gmail.com>
2026-03-22 17:34:04 -0300

    escena_principal

---
+++ tutorial-2d---actividad-4/main.gd
@@ -0,0 +1,52 @@
+extends Node
+@export var mob_scene: PackedScene
+var score
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start() 
+
+func _on_mob_timer_timeout() -> void:
+	# Crea una nuevo instancia de la escena Mob
+	var mob = mob_scene.instantiate()
+
+	# Elije una locación random en Path2D.
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	# Seteamos la ubicacion de mob a una posicion random
+	mob.position = mob_spawn_location.position
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
